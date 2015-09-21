6f61426db32f925fb048d6df8ce13d3def582ed9
Phil Johnston <support@mintplugins.com>
2015-09-21 14:49:42 -0400

    Admin Meta Scripts now enqueued only when needed.

---
+++ includes/misc-functions/admin-enqueue-scripts.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file contains the enqueue scripts function for the googlemaps plugin
+ *
+ * @since 1.0.0
+ *
+ * @package    MP Stacks GoogleMaps
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2015, Mint Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
+ */
